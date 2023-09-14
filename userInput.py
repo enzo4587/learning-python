b03fab69759ff36cdf774dc9ce0c94a8e871c870
Anzoe John <107835671+enzo4587@users.noreply.github.com>
2023-09-14 05:16:32 -0700

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,19 @@
+# user input
+
+number1 = input("enter first number: ")
+
+number2 = input("enter second number:")
+
+number1 = int(number1)
+
+number2 = int(number2)
+
+sum = number1 + number2
+
+print(sum)
+
+name = input("enter your name: ")
+
+name = str(name)
+
+print(name)
